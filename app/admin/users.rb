5d38187497bd8703b54249092e124482970ded68
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-10-10 16:46:40 +0300

    update active admin config, add users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,4 +2,14 @@
 
 ActiveAdmin.register User do
   permit_params :phone_number, :password, :password_confirmation, :name
+
+  form do |f|
+    f.inputs do
+      f.input :phone_number
+      f.input :password, required: f.object.new_record?
+      f.input :password_confirmation, required: f.object.new_record? if f.object.new_record?
+      f.input :name
+    end
+    f.actions
+  end
 end
