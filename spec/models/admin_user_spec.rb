90d8028e6cd6892ae82756351509e9a72529a504
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-10-10 23:43:40 +0300

    add AdminUser and Appointment models specs

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:admin_user) { create(:admin_user) }
+
+  describe 'database columns exists' do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+    it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
+    it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:remember_created_at).of_type(:datetime) }
+  end
+
+  describe 'database indexes exists' do
+    it { is_expected.to have_db_index(:email) }
+    it { is_expected.to have_db_index(:reset_password_token) }
+  end
 end
